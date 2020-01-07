921a3738301ca31e4361d537d42bb29a99404f8c
Samet Akcabay <samet.akcabay@gmail.com>
2020-01-07 16:03:42 +0100

    Init commit

---
+++ empridevops.mm
@@ -0,0 +1,92 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1565169098931" ID="ID_1740606763" MODIFIED="1569583284932" TEXT="EMPRI-DEVOPS">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569239272153" ID="ID_1600586889" MODIFIED="1569419572013" POSITION="left" STYLE="bubble" TEXT="Database Privacy">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569240361622" ID="ID_1852456129" MODIFIED="1569419572013" STYLE="bubble" TEXT="Differentiated Privacy">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569239318738" ID="ID_1514908100" LINK="DatabasePrivacyProtect/778_2006_Article_23.pdf" MODIFIED="1569419572013" STYLE="fork" TEXT="Protect Privacy with ACL ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1569240388405" ID="ID_854018213" LINK="DatabasePrivacyProtect/privacybook.pdf" MODIFIED="1569419572013" STYLE="fork" TEXT="Algorithmic Foundation ">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1569232267591" ID="ID_1652239125" MODIFIED="1569419572013" STYLE="bubble" TEXT="Database Obfuscation">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569232311893" ID="ID_1038476475" LINK="Patents/US8117221.pdf" MODIFIED="1569419572013" STYLE="fork" TEXT="Patent Gemalto">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1570445604973" ID="ID_14044691" MODIFIED="1570445613993" TEXT="Database Encryption">
+<node CREATED="1570439661075" ID="ID_504486410" LINK="DatabasePrivacyProtect/raluca-cryptdb.pdf" MODIFIED="1570614366733" STYLE="fork" TEXT="CryptDB">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1570445626166" ID="ID_281744884" MODIFIED="1570445653262" STYLE="fork" TEXT="full homomorphic encr."/>
+<node CREATED="1570626591245" ID="ID_823036128" MODIFIED="1570626604350" STYLE="fork" TEXT="format preserving encr."/>
+</node>
+<node CREATED="1569581421512" ID="ID_1377417429" MODIFIED="1570623822606" TEXT="Privacy Proxy/Perturbation">
+<node CREATED="1569581433815" ID="ID_535776533" LINK="https://github.com/TonicAI/masquerade" MODIFIED="1570626619889" STYLE="fork" TEXT="Masquerade"/>
+<node CREATED="1570523308620" ID="ID_1021424354" LINK="DatabasePrivacyProtect/diffix.pdf" MODIFIED="1570626629648" STYLE="fork" TEXT="Diffix">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1570523346938" ID="ID_142595446" LINK="DatabasePrivacyProtect/diffix-birch.pdf" MODIFIED="1570523361668" STYLE="fork" TEXT="Diffix-birch"/>
+<node CREATED="1570623828839" ID="ID_444263746" LINK="DatabasePrivacyProtect/p202-dinur.pdf" MODIFIED="1570623858869" STYLE="fork" TEXT="Statistical DB "/>
+</node>
+<node CREATED="1570624573316" ID="ID_434416633" LINK="DatabasePrivacyProtect/p20-olivier.pdf" MODIFIED="1570624603468" STYLE="fork" TEXT="Confidentiality, Integrity..."/>
+</node>
+<node CREATED="1569242970123" ID="ID_1618858312" MODIFIED="1569583283612" POSITION="right" STYLE="bubble" TEXT="Non tech Privacy ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569242991762" ID="ID_863007814" MODIFIED="1569419572013" STYLE="bubble" TEXT="CPM">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569243053803" ID="ID_1053777216" LINK="https://books.google.de/books?hl=de&amp;lr=&amp;id=8v89W_oJQ0wC&amp;oi=fnd&amp;pg=PR3&amp;dq=petronio+privacy&amp;ots=3Rj3k_oLvL&amp;sig=AYmUfOqSIxH4rQe0txjw_6TAbLs&amp;redir_esc=y#v=onepage&amp;q&amp;f=false" MODIFIED="1569419572013" STYLE="fork" TEXT="StandartWerk">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1569243162651" ID="ID_1283429353" LINK="Privacy/Miller-Weckert2000_Article_PrivacyTheWorkplaceAndTheInter.pdf" MODIFIED="1569419572013" STYLE="fork" TEXT="Work env">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1569411012460" ID="ID_616772260" LINK="Privacy/ChristenEtAl_IACAP-2015-Proceedings_final2.pdf" MODIFIED="1569419572012" STYLE="fork" TEXT="ethical pbdata in research">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1569410148141" ID="ID_595302013" MODIFIED="1569419572012" POSITION="right" STYLE="bubble" TEXT="Log Privacy ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569410187572" ID="ID_636349191" LINK="LogPrivacy/1802.05084.pdf" MODIFIED="1569419572012" STYLE="fork" TEXT="Anonymization replacement">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1569419358223" ID="ID_987883507" MODIFIED="1569419572012" STYLE="fork" TEXT="Anonymization encrypt.">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1569410333493" ID="ID_885617550" MODIFIED="1569419572012" POSITION="left" STYLE="bubble" TEXT="Privacy Design">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569410363553" ID="ID_761478584" LINK="privacyDesign/07527750.pdf" MODIFIED="1569419572012" STYLE="fork" TEXT="Critical Analysis of Strategies">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1569584013234" ID="ID_571811349" LINK="Privacy/a57-wagner.pdf" MODIFIED="1569584049457" STYLE="fork" TEXT="Privacy Metrics Survey"/>
+</node>
+<node CREATED="1569410808347" ID="ID_1180086101" MODIFIED="1569419572012" POSITION="right" STYLE="bubble" TEXT="Devs Privacy ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1569410821603" ID="ID_454607567" LINK="DevPrivacy/p20-robles.pdf" MODIFIED="1569419572012" STYLE="fork" TEXT="Identification mult. sources">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1569410872500" ID="ID_130847114" LINK="DevPrivacy/p97-venkataramani.pdf" MODIFIED="1569419572012" STYLE="fork" TEXT="Opensource expertise discovery">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1565172522306" ID="ID_1663766170" LINK="../Papyros/1802.05084.pdf" MODIFIED="1569419572011" STYLE="fork" TEXT=" Do Programmers Work at Night or During the Weekend?">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1569421888419" ID="ID_149355045" LINK="DevPrivacy/clusterdevdata.pdf" MODIFIED="1569421909952" STYLE="fork" TEXT="Clustering OpenSrc. dev data"/>
+<node CREATED="1569572104827" ID="ID_1630921750" MODIFIED="1569572104827" TEXT=""/>
+</node>
+<node CREATED="1570520435960" ID="ID_1000279040" MODIFIED="1570520441454" POSITION="left" STYLE="bubble" TEXT="Crypto">
+<node CREATED="1570520443484" ID="ID_1313506236" LINK="Crypto/Boldyreva2009_Chapter_Order-PreservingSymmetricEncry.pdf" MODIFIED="1570521468265" STYLE="fork" TEXT="Order preserving symmetric encr. "/>
+<node CREATED="1570521419658" ID="ID_1161560386" LINK="Crypto/searchingencr.pdf" MODIFIED="1570521432990" STYLE="fork" TEXT="Searching encr."/>
+</node>
+</node>
+</map>
